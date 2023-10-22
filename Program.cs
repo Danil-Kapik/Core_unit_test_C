44275dedf817dc4e8610fc832479203f737ce4bb
Daniil-Kapik <dann_kap@mail.ru>
2023-10-22 17:01:18 +0300

    feat: added the solution to the assignment

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputArray = { "apple", "cat", "dog", "banana", "car", "uu"};
+        List<string> outputList = new List<string>();
+
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                outputList.Add(str);
+            }
+        }
+        string[] outputArray = outputList.ToArray();
+        Console.WriteLine(string.Join(", ", outputArray));
+    }
+}
+
+
